e592a55f301b44f65713a922e0fa9e7c306253e5
Neuroen <phillip_morton@yahoo.com>
2022-11-11 13:24:46 +0100

    Drop Support for Windows

--- src/core/Starter.java
+++ src/core/Starter.java
@@ -1,10 +1,17 @@
 package core;
 
+import javax.swing.JOptionPane;
+
 public class Starter 
 {
 	public static void main(String[] args) 
 	{
-			Swing_View sw = new Swing_View();
-			sw.StartGui();
+		if(System.getProperty("os.name").contains("Windows"))
+		{
+			JOptionPane.showMessageDialog(null, "Windows wird nicht unterstützt.");
+			return;
+		}
+		Swing_View sw = new Swing_View();
+		sw.StartGui();
 	}
 }
